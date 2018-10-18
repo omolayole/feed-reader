bfe1d3cf3028605d6188738ae3ecbeba2933c8f1
omolayole <omolayoleogunyemi@gmail.com>
2018-10-18 10:34:58 +0100

    add a new test suite named New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -98,10 +98,30 @@ $(function() {
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* A new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let firstFeed,
+            secondFeed;
+
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+                firstFeed = document.querySelector('.feed').innerHTML;
+                done();
+            });
+            
+            loadFeed(1, function() {
+                secondFeed = document.querySelector('.feed').innerHTML;
+                done();
+            });
+         })
+
+         it('content changes', function() {
+            expect(firstFeed === secondFeed).toBe(false);
+         });
+    });
 }());
